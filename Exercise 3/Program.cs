f0f131c40a124ca5a1d79255e42107ab3e716381
Upper <goodp@yandex.ru>
2022-06-05 16:32:58 +0300

    ex 3

---
+++ Exercise 3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Написать программу копирования массива
+void PrintArray(int[] array)
+{
+    foreach(int digit in array)
+        Console.Write($"{digit} ");
+}
+int[] array = new int[10];
+int length = array.Length;
+int[] arrayCopy = new int[length];
+Random random = new Random();
+
+for (int i = 0; i < length; i++)
+    array[i] = random.Next(-100,100);
+
+Console.WriteLine("Массив:");
+PrintArray(array);
+Console.WriteLine();
+
+for (int j = 0; j < length; j++)
+   arrayCopy[j] = array[j]; 
+
+Console.WriteLine();
+Console.WriteLine("Скопированный массив:");
+PrintArray(arrayCopy);
