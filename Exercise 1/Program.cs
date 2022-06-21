9627f146fcf9c917ffb93c9ee30815183933289f
Upper <goodp@yandex.ru>
2022-06-21 15:29:09 +0300

    ex 1 remastered

--- Exercise 1/Program.cs
+++ Exercise 1/Program.cs
@@ -11,8 +11,11 @@ double k2=PrintValue("k2");
 double b1=PrintValue("b1");
 double b2=PrintValue("b2");
 
+if (k1==k2)
+Console.WriteLine("При таких значениях прямые не пересекаются");
+else
+{
 double x=(b1-b2)/(k1-k2);
 double y=k2*x+b2;
-
 Console.WriteLine($"Координаты точки пересечения для k1 = {k1}, b1 = {b1}, k2 = {k2}, b2 = {b2} равны х = {x} и y = {y}.");
-
+}
